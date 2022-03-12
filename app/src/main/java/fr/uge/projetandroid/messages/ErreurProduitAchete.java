3cde2442cc563c3dafe2789905e19f230dbe1bd0
Nouhaila Rais <nouhaila.rais@edu.univ-eiffel.fr>
2022-03-12 01:19:09 +0100

    update layout, msg

---
+++ app/src/main/java/fr/uge/projetandroid/messages/ErreurProduitAchete.java
@@ -0,0 +1,15 @@
+package fr.uge.projetandroid.messages;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import fr.uge.projetandroid.R;
+
+public class ErreurProduitAchete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erreur_produit_achete);
+    }
+}
